50430d3f41e95030c6a1a6ee494a0088f11e90c3
Mesquita <mesquita@aospa.co>
2025-04-17 15:51:31 -0300

    components: ButtonLinks: Added Deezer as streaming service
     * Also added streaming links
    
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/components/ButtonLinks.vue
+++ src/components/ButtonLinks.vue
@@ -20,34 +20,41 @@ const iconUri = (icon: string) => `${window.location.origin}${icon}`
 const platforms = [
   {
     name: 'Spotify',
-    url: 'https://open.spotify.com/',
+    url: 'https://open.spotify.com/intl-pt/track/0G2S9ho5OUE4jbDlmoUZ16?si=14fba82c177b4b5a',
     color: '#1DB954',
     icon: 'icons/spotify.svg',
   },
   {
     name: 'YouTube',
-    url: 'https://youtube.com/',
+    url: 'https://youtu.be/wV11IuxSCd0',
     color: '#FF0000',
     icon: 'icons/youtube.svg',
   },
   {
     name: 'YouTube Music',
-    url: 'https://music.youtube.com/',
+    url: 'https://music.youtube.com/watch?v=W5X6eg8eIq8&si=ed8Qn40JQavjaT8u',
     color: '#FF0000',
     icon: 'icons/youtube-music.svg',
   },
   {
     name: 'Amazon Music',
-    url: 'https://music.amazon.com/',
+    url: 'https://music.amazon.com.br/tracks/B0F4RS6CYL?marketplaceId=A2Q3Y263D00KWC&musicTerritory=BR&ref=dm_sh_SWWh7vk7nSdnUy6REzua4HKVF',
     color: '#232F3E',
     icon: 'icons/amazon-music.svg',
   },
   {
     name: 'Apple Music',
-    url: 'https://music.apple.com/',
+    url: 'https://music.apple.com/br/song/escandalosa-gra%C3%A7a/1808512338',
     color: '#FA233B',
     icon: 'icons/apple-music.svg',
   },
+  {
+    name: 'Deezer',
+    url: 'https://dzr.page.link/5jtNiggfoajhaZBp7',
+    color: '#a238ff',
+    icon: 'icons/deezer.svg',
+  }
+
 ]
 </script>
 
